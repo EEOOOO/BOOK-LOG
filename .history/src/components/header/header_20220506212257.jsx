80229958dd19ed8d55151ea8d8e93130ccbf8572
WooYoon-local <dndbs14@gmail.com>
2022-05-06 21:28:08 +0900

    design: styling login page

--- .history/src/components/header/header_20220506212257.jsx
+++ .history/src/components/header/header_20220506212257.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './header.module.css';
 const Header = ({onLogout}) => (
     <header className={styles.header}>
-        {false && <button className={styles.logout} onClick={onLogout}>Logout</button>}
+        {true && <button className={styles.logout} onClick={onLogout}>Logout</button>}
         <img src="/images/logo.png" className={styles.logoImg} />
         {false && <h1 className={styles.logoTitle}>Reading List Maker</h1>}
     </header>
